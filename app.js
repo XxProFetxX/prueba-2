c8311b12db06357d088aca3e6374e4fa117e26cc
XxProFEtxX <ogreydi123@gmail.com>
2023-09-26 09:40:05 -0500

    feat: checkbox details

--- app.js
+++ app.js
@@ -15,3 +15,29 @@ const to_do = [
     'Comprar entradas para el cine',
 ]
 
+const todoListContainer = document.querySelector('.js-cont-todo');
+const ul = GI.createElement('ul');
+todoListContainer.appendChild(ul);
+
+to_do.forEach((itemText) => {
+    const li = GI.createElement('li');
+    const checbox = GI.createElement('input', {
+        type: 'checkbox',
+        onchange: function() {
+            if (this.checbox) {
+                li.style.textDecoration = 'line-through';
+                li.style.color = 'gray';
+            } else {
+                li.style.textDecoration = 'none';
+                li.style.color = 'initial';
+            }
+        }
+    });
+
+    const label = GI.createElement('label', { htmlFor: itemText });
+    label.textContent = itemText;
+
+    li.appendChild(checbox);
+    li.appendChild(label);
+    ul.appendChild(li);
+})
