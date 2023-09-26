942639f8e3367b69a3edf8d944feb792913f03d6
XxProFEtxX <ogreydi123@gmail.com>
2023-09-26 09:44:25 -0500

    feat: add check input

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-'use strict'
+'use strict';
+
 const GI = {
-	createElement (tag, atributes) {
-		let element = document
-		return document.createElement(tag)
-	}
+  createElement(tag, attributes) {
+    let element = document.createElement(tag);
+    if (attributes) {
+      for (let key in attributes) {
+        element.setAttribute(key, attributes[key]);
+      }
+    }
+    return element;
+  }
 }
 
-
 const to_do = [
-    'Alimentar al perro',
-    'Estudiar para prueba escrita',
-    'Pagar taxes',
-    'Pasear a perro',
-    'Comprar entradas para el cine',
-]
+  'Alimentar al perro',
+  'Estudiar para prueba escrita',
+  'Pagar taxes',
+  'Pasear a perro',
+  'Comprar entradas para el cine',
+];
 
 const todoListContainer = document.querySelector('.js-cont-todo');
 const ul = GI.createElement('ul');
 todoListContainer.appendChild(ul);
 
 to_do.forEach((itemText) => {
-    const li = GI.createElement('li');
-    const checbox = GI.createElement('input', {
-        type: 'checkbox',
-        onchange: function() {
-            if (this.checbox) {
-                li.style.textDecoration = 'line-through';
-                li.style.color = 'gray';
-            } else {
-                li.style.textDecoration = 'none';
-                li.style.color = 'initial';
-            }
-        }
-    });
+  const li = GI.createElement('li');
+  const checkbox = GI.createElement('input', {
+    type: 'checkbox',
+  });
+  const label = GI.createElement('label');
+  label.textContent = itemText;
 
-    const label = GI.createElement('label', { htmlFor: itemText });
-    label.textContent = itemText;
+  li.appendChild(checkbox);
+  li.appendChild(label);
+  ul.appendChild(li);
 
-    li.appendChild(checbox);
-    li.appendChild(label);
-    ul.appendChild(li);
-})
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      label.style.textDecoration = 'line-through';
+      label.style.color = 'gray';
+    } else {
+      label.style.textDecoration = 'none';
+      label.style.color = 'initial';
+    }
+  });
+});
